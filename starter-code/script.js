91aa2d0abcf28988d1d5eca1bbb9f82f18d992ea
Mabchir <38863638+Mabchir@users.noreply.github.com>
2022-03-25 14:28:03 -0400

    desktop responsiveness + fixing hover

--- starter-code/script.js
+++ starter-code/script.js
@@ -1,5 +1,14 @@
-$(window).resize(function () {
+//
+
+$(document).ready(function () {
   if ($(window).width() > 800) {
     $(".firstpart").insertAfter(".icon1");
   }
+  $(window).resize(function () {
+    if ($(window).width() <= 800) {
+      $(".firstpart").insertAfter(".bubbleImg");
+    } else {
+      $(".firstpart").insertAfter(".icon1");
+    }
+  });
 });
